00a2ca12c7431043b21a88c1fceea12bad8bc0ca
utkinia <ivanautkin@gmail.com>
2024-01-17 18:23:45 +0300

    lab_1.3

--- Lab1.3/snmp.py
+++ Lab1.3/snmp.py
@@ -4,21 +4,21 @@ IP="10.31.70.209"
 PORT=161
 DESCR_MIB="sysDescr"
 IFACES_MIB="1.3.6.1.2.1.2.2.1.2"
+COMMUNITY="public"
+
+def snmp_human_data(snmp_data):
+    for i in snmp_data:
+        for j in i[3]:
+            print(j)
 
 snmp_name = ObjectIdentity('SNMPv2-MIB',DESCR_MIB,0)
 snmp_interfaces = ObjectIdentity(IFACES_MIB)
 
-descr = getCmd(SnmpEngine(), CommunityData('public', mpModel=0), UdpTransportTarget((IP, PORT)), ContextData(), ObjectType(snmp_name))
-ifaces = nextCmd(SnmpEngine(), CommunityData('public', mpModel=0), UdpTransportTarget((IP, PORT)), ContextData(), ObjectType(snmp_interfaces),lexicographicMode=False)
+descr = getCmd(SnmpEngine(), CommunityData(COMMUNITY, mpModel=0), UdpTransportTarget((IP, PORT)), ContextData(), ObjectType(snmp_name))
+ifaces = nextCmd(SnmpEngine(), CommunityData(COMMUNITY, mpModel=0), UdpTransportTarget((IP, PORT)), ContextData(), ObjectType(snmp_interfaces),lexicographicMode=False)
 
 print("Description:")
-for i in descr:
-    for j in i[3]:
-        print(j)
-
+snmp_human_data(descr)
 print("")
-
 print("Interfaces:")
-for i in ifaces:
-    for j in i[3]:
-        print(j)
+snmp_human_data(ifaces)
