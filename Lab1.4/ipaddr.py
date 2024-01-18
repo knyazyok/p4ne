c4baf78653b7050b37af7b3e0a33c2fc2a55cae7
utkinia <ivanautkin@gmail.com>
2024-01-18 18:11:28 +0300

    lab_1.3

---
+++ Lab1.4/ipaddr.py
@@ -0,0 +1,25 @@
+import random
+from ipaddress import IPv4Network
+
+class IPv4RandomNetwork(IPv4Network):
+    def __init__(self,):
+        IPv4Network.__init__(self,(random.randint(0x0b000000,0xdf000000),random.randint(8,24)),False)
+    def key_value(self):
+        return int(str(int(self.netmask))+str(int(self.network_address)))
+    def regular(self):
+        if self.is_global():
+            return True
+        else:
+            return False
+
+def sort_key(i):
+    return i.key_value
+
+nets=[]
+for i in range(0,100):
+    net = IPv4RandomNetwork()
+    if net.regular:
+        nets.append(net)
+
+for i in sorted(nets):
+    print(i)
