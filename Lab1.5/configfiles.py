2119a6ef4f486e6b9e95f5897f44c6033353ece0
utkinia <ivanautkin@gmail.com>
2024-01-19 17:03:45 +0300

    lab_1.3

--- Lab1.5/configfiles.py
+++ Lab1.5/configfiles.py
@@ -4,9 +4,15 @@ ip_addr=[]
 for i in glob.glob("C:\\Users\\utkinia\\Documents\\Python\\config_files\\*.log"):
     f=open(i)
     for string in f:
-        if string.find("ip address") != -1 & string.find("no") == -1:
-            string=string.strip().replace("ip address","")
-            ip_addr.append(string)
+        if string.find("ip address") != -1 and string.find("no") == -1:
+            string=string.replace("ip address","").strip()
+            elements=string.split(" ")
+            for i in elements:
+                if not i[0] in "0123456789":
+                    elements.remove(i)
+            print(elements)
+#            ipaddr = " ".join(elements)
+#            ip_addr.append(ipaddr)
 
 for i in sorted(list(set(ip_addr))):
     print(i)
