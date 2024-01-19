afb9e2fe40f8ba35a9bb50a12a99688cac0010ed
utkinia <ivanautkin@gmail.com>
2024-01-19 23:13:17 +0300

    lab_1.3

--- Lab1.6/regexp.py
+++ Lab1.6/regexp.py
@@ -1,4 +1,28 @@
-import re
+import re,ipaddress,glob
 
-m = re.match("^ip address ((?:[0-9]{1,3}\.?){4}) ((?:[0-9]{1,3}\.?){4})", "ip address 8.8.8.4 255.255.255.255")
-print(m.groups())
+LOG_PATH="C:\\Users\\utkinia\\Documents\\Python\\config_files"
+def str_to_ip(ipstr):
+    if re.search("ip address", ipstr):
+        net_objects = re.search("([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})", ipstr)
+        if net_objects:
+            addr = ipaddress.IPv4Interface(net_objects.groups())
+            return addr
+        else:
+            return None
+    else:
+        return None
+
+def sort_key(i):
+    return int(i.ip) + (int(i.network.netmask) * 2**32)
+
+
+all_address=[]
+for i in glob.glob(LOG_PATH+"\\*.log"):
+    f = open(i)
+    for j in f:
+        a = str_to_ip(j)
+        if a != None:
+            all_address.append(a)
+
+for i in sorted(all_address,key=sort_key):
+    print(i)
