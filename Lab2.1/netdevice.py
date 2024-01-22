b90441acadee4f027b7978345b155d5e5602493b
utkinia <ivanautkin@gmail.com>
2024-01-22 16:17:52 +0300

    lab_1.3

---
+++ Lab2.1/netdevice.py
@@ -0,0 +1,30 @@
+import paramiko,requests,time,re
+
+CONSOLE_IP = "10.31.70.209"
+REST_IP = "10.31.70.210"
+LOGIN = "restapi"
+PASSWORD = "j0sg1280-7@"
+REST_PORT = "55443"
+
+BUF_SIZE = 20000
+TIMEOUT = 1
+
+ssh_connection = paramiko.SSHClient()
+ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh_connection.connect(CONSOLE_IP, username=LOGIN, password=PASSWORD, look_for_keys=False, allow_agent=False)
+session = ssh_connection.invoke_shell()
+
+session.send("\n")
+session.recv(BUF_SIZE)
+session.send("terminal length 0\n")
+time.sleep(TIMEOUT)
+
+session.send("\n")
+session.recv(BUF_SIZE)
+session.send("show interfaces\n")
+time.sleep(TIMEOUT*2)
+s = session.recv(BUF_SIZE).decode()
+
+session.close()
+
