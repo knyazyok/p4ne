be0f0d18029059eecac92cff93be46241d61e5f2
utkinia <ivanautkin@gmail.com>
2024-01-23 16:14:13 +0300

    lab_2.1

--- Lab2.1/netdevice_rest.py
+++ Lab2.1/netdevice_rest.py
@@ -1,57 +1,10 @@
-import pprint
-
-import paramiko,requests,time,re,pandas
+import pprint,requests,re,pandas
 
 IP = "10.31.70.209"
 REST_IP = "10.31.70.209"
 LOGIN = "restapi"
 PASSWORD = "j0sg1280-7@"
 
-BUF_SIZE = 20000
-TIMEOUT = 1
-
-ssh_connection = paramiko.SSHClient()
-ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-ssh_connection.connect(IP, username=LOGIN, password=PASSWORD, look_for_keys=False, allow_agent=False)
-session = ssh_connection.invoke_shell()
-
-session.send("\n")
-session.recv(BUF_SIZE)
-session.send("terminal length 0\n")
-time.sleep(TIMEOUT)
-
-session.send("\n")
-session.recv(BUF_SIZE)
-session.send("show interfaces\n")
-time.sleep(TIMEOUT*2)
-s = session.recv(BUF_SIZE).decode()
-
-session.close()
-
-print("Paramiko data")
-iface_list = [["Name","Input Packets","Input Bytes","Output Packets","Output Bytes"]]
-out = s.split("\n")
-for i in out:
-    if i[0] != " " and not re.search("show interfaces",i) and re.search("is",i):
-        name = i.split()[0]
-    elif re.search("packets input",i):
-        ipackets = i.split()[0]
-        ibytes = i.split()[3]
-    elif re.search("packets output",i):
-        opackets = i.split()[0]
-        obytes = i.split()[3]
-        iface_list.append([name,ipackets,ibytes,opackets,obytes])
-#for i in iface_list:
-#    print("\t\t\t\t".join(i))
-
-df = pandas.DataFrame(iface_list)
-
-print(df)
-
-print()
-
-print("REST API data")
 headers = {
     "accept": "application/yang-data+json",
     "Content-Type": "application/yang-data+json"
