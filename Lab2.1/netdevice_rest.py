b937bb3201a3d77e1475395f4f5ceb4f316c3611
utkinia <ivanautkin@gmail.com>
2024-01-23 16:38:24 +0300

    lab_2.1

--- Lab2.1/netdevice_rest.py
+++ Lab2.1/netdevice_rest.py
@@ -11,5 +11,12 @@ headers = {
 }
 requests.packages.urllib3.disable_warnings()
 r = requests.get("https://" + IP + '/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces', auth=(LOGIN, PASSWORD), headers=headers, verify=False)
+iface_list = [["Name","Input Packets","Input Bytes","Output Packets","Output Bytes"]]
 for i in r.json()['Cisco-IOS-XE-interfaces-oper:interfaces']['interface']:
-    print(i['name'])
+    in_packets_total=int(i['statistics']['in-broadcast-pkts'])+int(i['statistics']['in-multicast-pkts'])+int(i['statistics']['in-unicast-pkts'])
+    out_packets_total = int(i['statistics']['out-broadcast-pkts'])+int(i['statistics']['out-multicast-pkts'])+int(i['statistics']['out-unicast-pkts'])
+    iface_list.append([i['name'],in_packets_total,int(i['statistics']['in-octets']),out_packets_total,int(i['statistics']['out-octets'])])
+
+df = pandas.DataFrame(iface_list)
+
+print(df)
